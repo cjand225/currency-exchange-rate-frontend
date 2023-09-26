ab3fe4d8be82beab7a501d6e6b4af8e84ab090ca
James Andrews <cjand225@gmail.com>
2023-09-26 16:03:08 -0600

    Updated page entry point on page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+import App from "./components/App";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
-        <h1>Currency Exchange</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Currency Exchange
+      </h1>
+      <App></App>
     </main>
   );
 }
