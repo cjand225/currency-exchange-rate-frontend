57dda8fb34f1e41652e9fd15f07b6485f559ef83
James Andrews <cjand225@gmail.com>
2023-10-20 11:41:14 -0600

    Updated page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-        Currency Exchange
+        CAD Exchange Rates
       </h1>
       <App></App>
     </main>
